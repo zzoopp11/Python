7b94dbb3fde8b2b799ceac933c2928f658e338a3
zzoopp11 <153420824+zzoopp11@users.noreply.github.com>
2024-02-15 23:53:28 +0300

    Add files via upload

---
+++ ООП.Лабораторная работа 3/task_3.py
@@ -0,0 +1,68 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    """ Бумажная книга. """
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self._pages = pages
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages: int) -> None:
+        if not isinstance(new_pages, int):
+            raise TypeError("Переменная 'pages' должна быть типа 'int'.")
+        if new_pages < 0:
+            raise ValueError("Переменная 'pages' должна быть положительной.")
+        self._pages = new_pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    """ Аудио книга. """
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self._duration = duration
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float) -> None:
+        if not isinstance(new_duration, float):
+            raise TypeError("Переменная 'duration' должна быть типа 'float'.")
+        if new_duration < 0:
+            raise TypeError("Переменная 'duration' должна быть положительной.")
+        self._duration = new_duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
+
+
+
+
